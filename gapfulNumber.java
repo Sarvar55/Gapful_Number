8d0e4d31bd965cd8157ad8e22fdad150ea58c283
Sarvar Musazade <76266468+Sarvar55@users.noreply.github.com>
2021-10-18 10:13:11 +0300

    Update gapfulNumber.java

--- gapfulNumber.java
+++ gapfulNumber.java
@@ -3,29 +3,30 @@ import java.util.Scanner;
 public class gapfulNumber {
 
 	public static void main(String[] args) {
+        // TODO Auto-generated method stub
          Scanner scn = new Scanner(System.in);
-         System.out.println("Place Enter any number:");
+         System.out.print("Please Enter any number: ");
          int number = scn.nextInt();
+         
+         
          if(gapfulNumber(firstNumber(number), lastNumber(number), number)) 
             System.out.print("Gapful Number");
          else 
-            System.out.print("Is not Gapful Number"); 
-	
+            System.out.print("It is not a Gapful Number"); 
+	}
 	
 	public static int firstNumber(int number) {
-	   int digit = (int)  Math.log10(number);
-           return (int) (number / Math.pow(10, number));
+		int digits = (int)(Math.log(number) / Math.log(10));
+                return (int)(number / Math.pow(10, digits));
 	}
 	
 	public static int lastNumber(int number) {
-	  String stringNumber = String.valueOf(number);
-          return (int) stringNumber.charAt(stringNumber.length() - 1);
+		return number % 10;
 	}
 	
 	public static boolean gapfulNumber(int firstNumber,int lastNumber,int number) {
-	  int concatNumber = firstNumber * 10 + lastNumber;
-	  return (number % concatNumber == 0) ? true : false;	
-       }	
-    }
+		int concatNumber = firstNumber * 10 + lastNumber;
+		return (number % concatNumber == 0) ? true : false;	
+	}
 
 }
